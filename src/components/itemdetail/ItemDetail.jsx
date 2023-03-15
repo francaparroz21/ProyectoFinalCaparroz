abeb8a21cc23f41e9a2619c2e8fefd7f69c2a7ea
francaparroz21 <francaparroz21@gmail.com>
2023-03-15 20:49:15 -0300

    item detail creado

---
+++ src/components/itemdetail/ItemDetail.jsx
@@ -0,0 +1,31 @@
+import { ItemCount } from "../itemcount/ItemCount";
+import { useCartContext } from "../../context/cartContext/CartContext";
+
+export const ItemDetail = ({ data }) => {
+
+    //Add product del cart context
+    const { addProduct } = useCartContext()
+
+    //para que pase un valor (callback) el item count.
+    const onAdd = (quantity) => {
+        addProduct(data, quantity)
+    }
+    return (
+        <>
+            <div className="item-detail-container container">
+                <h3 className="title-detail-container">{data.name}</h3>
+
+                <div className="display-detail-container">
+                    <img className="img-detail-container" src={data.urlImg} alt={data.name} />
+                    <div className="display-right-detail-container">
+                        <p>{data.description}</p>
+                        <span>Stock disponible: {data.stock}</span>
+                        <div className='add-tocart'>
+                            <ItemCount count={1} onAdd={onAdd} data={data} stockProduct={data.stock} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/components/itemdetailcontainer/ItemDetailContainer.jsx
+++ src/components/itemdetailcontainer/ItemDetailContainer.jsx
@@ -2,9 +2,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../firebaseConfig/firebase";
 import { doc, getDoc } from "firebase/firestore";
-import { ItemCount } from "../itemcount/ItemCount";
 import "./itemdetailcontainer.css"
-import { useCartContext } from "../../context/cartContext/CartContext";
+import { ItemDetail } from "../itemdetail/ItemDetail";
 
 export const ItemDetailContainer = () => {
 
@@ -12,15 +11,6 @@ export const ItemDetailContainer = () => {
     const [data, setData] = useState({});
     // useParams /item/:id
     const { id } = useParams()
-
-    //Add product del cart context
-    const { addProduct } = useCartContext()
-
-
-    //para que pase un valor (callback) el item count.
-    const onAdd = (quantity) =>{
-        addProduct(data,quantity)
-    }
 
     useEffect(() => {
         const docRef = doc(db, "bossyProducts", id)
@@ -31,21 +21,6 @@ export const ItemDetailContainer = () => {
 
 
     return (
-        <>
-            <div className="item-detail-container container">
-                <h3 className="title-detail-container">{data.name}</h3>
-
-                <div className="display-detail-container">
-                    <img className="img-detail-container" src={data.urlImg} alt={data.name} />
-                    <div className="display-right-detail-container">
-                        <p>{data.description}</p>
-                        <span>Stock disponible: {data.stock}</span>
-                        <div className='add-tocart'>
-                            <ItemCount count={1} onAdd={onAdd} data={data} stockProduct={data.stock} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <ItemDetail data={data}/>
     )
 }
